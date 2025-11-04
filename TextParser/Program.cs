0d9aebe8e3ec132f7c621200bbb80aec9ca8652b
IlsIkYoon <yis9595@gmail.com>
2025-11-04 18:28:38 +0900

    made string read method

--- TextParser/Program.cs
+++ TextParser/Program.cs
@@ -3,16 +3,21 @@
 
 //main에선 Parser 테스트 확인 정도 하기
 
+using System.Diagnostics;
+
 CTextParser parser = new CTextParser();
 
 parser.OpenTextFile("parsertest.txt");
 
 int number;
 int good;
+string str = "";
 parser.SearchData<int>(out number, "number");
 parser.SearchData<int>(out good, "good");
+parser.SearchData(out str, "str");
+
 
 Console.WriteLine($"number : {number}");
 Console.WriteLine($"good : {good}");
-
+Console.WriteLine($"str : {str}");
 
--- TextParser/TextParser.cs
+++ TextParser/TextParser.cs
@@ -22,14 +22,6 @@ class CTextParser
         return true;
     }
 
-    public void CloseTextFile()
-    {
-        //파일 스트림 종료
-
-    
-    }
-
-
     public bool SearchData<T>(out T dest, string textName) where T : struct
     {
         _bufferPos = 0;
@@ -37,13 +29,13 @@ class CTextParser
 
         while (true)
         {
-            if (_bufferPos >= targetLen - 1)
+            if (IsBufferPosEnd())
             {
                 dest = default(T);
                 return false;
             }
 
-            SkipNoneCommand(); //유효한 문자열까지 점프
+            SkipNoneCommand();
             string saveBuf = "";
             while (true)
             {
@@ -52,9 +44,7 @@ class CTextParser
 
                 _bufferPos++;
 
-                if (_buffer[_bufferPos] == ' ' || _buffer[_bufferPos] == '\n' || _buffer[_bufferPos] == 0x09 || _buffer[_bufferPos] == 0x08 ||
-                    _bufferPos >= targetLen - 1 ||
-                    (_buffer[_bufferPos] == '/' && _buffer[_bufferPos + 1] == '/') || (_buffer[_bufferPos] == '/' && _buffer[_bufferPos + 1] == '*'))
+                if (IsNotChar())
                 {
                     break;
                 }
@@ -72,11 +62,10 @@ class CTextParser
                 _bufferPos++;
                 SkipNoneCommand();
 
-                if (_buffer[_bufferPos] >= 0x30 && _buffer[_bufferPos] <= 0x39) //첫 문자가 정수의 범위에 들어있다면 숫자로 간주
+                if (IsNumberData()) //첫 문자가 정수의 범위에 들어있다면 숫자로 간주
                 {
                     string numBuf = "";
-                    int i = 0;
-                    while (_buffer[_bufferPos] >= 0x30 && _buffer[_bufferPos] <= 0x39)
+                    while (IsNumberData())
                     {
                         numBuf += (char)_buffer[_bufferPos];
                         _bufferPos++;
@@ -94,43 +83,170 @@ class CTextParser
                     return true;
                 }
             }
+
         }
 
         return true;
     }
 
-    private void SkipNoneCommand()
+
+    public bool SearchData(out string dest, string textName)
     {
-	while (true) {
-		if (_buffer[_bufferPos] == ' ' || _buffer[_bufferPos] == '\n' || _buffer[_bufferPos] == 0x09 || _buffer[_bufferPos] == 0x08)
-		{
-            _bufferPos++;
-		}
-		else if (_buffer[_bufferPos] == '/' && (_buffer[_bufferPos] + 1) == '/')
-		{
-			while (_buffer[_bufferPos] != '\n') {
-				_bufferPos++;
-			}
+        _bufferPos = 0;
+        int targetLen = _buffer.Length;
 
-		}
-		else if (_buffer[_bufferPos] == '/' && _buffer[_bufferPos+ 1] == '*') {
-			while (_buffer[_bufferPos - 1] != '*' || _buffer[_bufferPos] != '/')
-			{
-				_bufferPos++;
+        while (true)
+        {
+            if (IsBufferPosEnd())
+            {
+                dest = null;
+                return false;
+            }
 
-				if (_bufferPos >= _buffer.Length - 1) {
-                        // 주석 해제 문자가 없는데 버퍼의 끝에 도달하면 오류로 간주
-                        Debugger.Break();
-					return;
-				}
-			}
-			_bufferPos++;
-		}
-		else {
-			break;
-		}
-	}
+            SkipNoneCommand();
+            string saveBuf = "";
+            while (true)
+            {
+                //문자열을 받아서 저장하고 
+                saveBuf += (char)_buffer[_bufferPos];
+                _bufferPos++;
+
+                if (IsNotChar())
+                {
+                    break;
+                }
+            }
+
+            if (saveBuf != textName)
+            {
+                continue;
+            }
+
+            SkipNoneCommand();
+
+            if (_buffer[_bufferPos] == '=')
+            {
+                _bufferPos++;
+                SkipNoneCommand();
+
+                if (_buffer[_bufferPos] == '"')
+                {
+                    string strBuf = "";
+                    _bufferPos++;
+                    while (_buffer[_bufferPos] != '"')
+                    {
+                        if (IsBufferPosEnd()) //도중에 버퍼에 끝에 도달했다면
+                        {
+                            Debugger.Break();
+                            dest = null;
+                            return false;
+                        }
+
+                        strBuf += (char)_buffer[_bufferPos];
+                        _bufferPos++;
+                    }
+                    dest = strBuf;
+                    return true;
+                }
+            }
+
+        }
+
+        return true;
     }
 
+    /// <summary>
+    /// 유효한 문자가 나올 때 까지 커서를 이동시켜 줌
+    /// </summary>
+    private void SkipNoneCommand()
+    {
+        while (true)
+        {
+            if (_buffer[_bufferPos] == ' ' || _buffer[_bufferPos] == '\n' || _buffer[_bufferPos] == 0x09 || _buffer[_bufferPos] == 0x08)
+            {
+                _bufferPos++;
+            }
+            else if (_buffer[_bufferPos] == '/' && (_buffer[_bufferPos] + 1) == '/')
+            {
+                while (_buffer[_bufferPos] != '\n')
+                {
+                    _bufferPos++;
+                }
+
+            }
+            else if (_buffer[_bufferPos] == '/' && _buffer[_bufferPos + 1] == '*')
+            {
+                while (_buffer[_bufferPos - 1] != '*' || _buffer[_bufferPos] != '/')
+                {
+                    _bufferPos++;
+
+                    if (_bufferPos >= _buffer.Length - 1)
+                    {
+                        // 주석 해제 문자가 없는데 버퍼의 끝에 도달하면 오류로 간주
+                        Debugger.Break();
+                        return;
+                    }
+                }
+                _bufferPos++;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    /// <summary>
+    /// 버퍼 포인터가 끝에 도달했는지를 반환
+    /// </summary>
+    /// <returns></returns>
+    private bool IsBufferPosEnd()
+    {
+        if (_bufferPos >= _buffer.Length - 1)
+        {
+            return true;
+        }
+
+        return false;
+    }
+    /// <summary>
+    /// 문자가 숫자에 해당하는지를 반환
+    /// </summary>
+    /// <returns></returns>
+    private bool IsNumberData()
+    {
+        if (_buffer[_bufferPos] >= 0x30 && _buffer[_bufferPos] <= 0x39)
+        {
+            return true;
+        }
+
+        return false;
+    }
+    /// <summary>
+    /// BufferPos가 문자를 가리키지 않다면 true
+    /// </summary>
+    /// <returns></returns>
+    private bool IsNotChar()
+    {
+        if (_buffer[_bufferPos] == ' ' || _buffer[_bufferPos] == '\n' || _buffer[_bufferPos] == 0x09 || _buffer[_bufferPos] == 0x08 ||
+                   IsBufferPosEnd() ||
+                   (_buffer[_bufferPos] == '/' && _buffer[_bufferPos + 1] == '/') || (_buffer[_bufferPos] == '/' && _buffer[_bufferPos + 1] == '*'))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool IsSemiColon()
+    {
+        if (_buffer[_bufferPos] == ';')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
 }
